2503507d4cc425c3147361fc37f589ab3b19321e
sugimoto <sugimoto@prompt1.com>
2025-04-21 21:09:15 +0900

    feat: サービスワーカーファイルを追加

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,20 +1,25 @@
-importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging.js");
+importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyCHXDvCgn1JLfJokiMflPPhu5n5CSOyw2w",
+  authDomain: "fcm-notification-demo-71005.firebaseapp.com",
+  projectId: "fcm-notification-demo-71005",
+  messagingSenderId: "239882082991",
+  appId: "1:239882082991:web:4cbf27e851da2ee044525a",
 });
 
 const messaging = firebase.messaging();
 
+// バックグラウンドでのメッセージ受信時の処理
 messaging.onBackgroundMessage((payload) => {
-  console.log("バックグラウンド通知受信:", payload);
-  self.registration.showNotification(payload.notification.title, {
+  console.log('Received background message:', payload);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
     body: payload.notification.body,
-    icon: payload.notification.icon,
-  });
+    icon: payload.notification.icon
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
